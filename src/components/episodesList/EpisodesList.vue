28643720d3781deba5b5211feb386f28d9624e79
kamil stawski <stawski.kamil@gmail.com>
2018-12-08 13:44:32 +0100

    get episodes, show episode item

--- src/components/episodesList/EpisodesList.vue
+++ src/components/episodesList/EpisodesList.vue
@@ -4,13 +4,14 @@
 
         <div class="episodes__search">
             <!-- search icon -->
-            
+            <SearchIcon />
             <input type="text" placeholder="Search">
         </div>
 
         <ul class="episodes__list">
-            <li>
+            <li v-for="episode in episodes" :key="episode.id" >
                 <!-- Episode Item -->
+                <EpisodeListItem :episode="episode" />
             </li>
             <!-- ... -->
         </ul>
@@ -22,11 +23,31 @@
 </template>
 
 <script>
+import SearchIcon from '@/assets/icon-search.svg'
+import EpisodeListItem from '@/components/episodesListItem/EpisodesListItem'
+
 export default {
     data() {
         return {
-            isLoading : false
+            isLoading : false,
+            episodes: [],
+            page: 1
         } 
+    },
+    components: {
+        SearchIcon,
+        EpisodeListItem
+    },
+    created() {
+        fetch('http://tiny-rick.tk/api/episode/')
+            .then( (data) => data.json())
+            .then( (data) => {
+                this.episodes = data.results
+                console.log(data.results);
+            })
+    },
+    methods: {
+        
     }
 }
 </script>
---
+++ src/components/episodesListItem/EpisodesListItem.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="episode-item">
+        <h3 class="episode-item__num">Episode {{getEpisode}}</h3>
+        <h2 class="episode-item__name">{{episode.name}}</h2>
+        <span class="episode-item__season-badge">Season {{getSeason}}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    computed: {
+        getSeason() {
+            return Number(this.episode.episode.substring(2,3))
+        },
+        getEpisode() {
+            return Number(this.episode.episode.substring(4,6))
+        }
+    },
+    props: {
+        episode: {
+            type: Object
+        }
+    }
+}
+</script>
