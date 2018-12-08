145e0f2161ffb992683e786674842aaffb805152
kamil stawski <stawski.kamil@gmail.com>
2018-12-08 12:38:05 +0100

    Create TheHeader.vue

---
+++ src/components/layout/TheHeader/TheHeader.vue
@@ -0,0 +1,45 @@
+<template>
+    <header class="header">
+        <a href="/" class="header__logo">
+            <!-- Logo -->
+            <Logo />
+        </a>
+        
+        <button @click="toggleNav" class="header__nav-trigger">
+            <!-- Hamburger Icon -->
+            <Hamburger />
+        </button>
+
+        <nav class="header__nav" :class="{ 'is-visible' : isVisible}">
+            <a class="header__nav-link" href="/">
+            Episodes
+            </a>
+            <a class="header__nav-link" href="/quiz">
+            Quiz
+            </a>
+        </nav>
+    </header>
+</template>=
+
+<script>
+
+import Logo from '@/assets/logo.svg';
+import Hamburger from '@/assets/icon-menu.svg';
+
+export default {
+  components: {
+    Logo,
+    Hamburger
+  },
+  data() {
+      return {
+          isVisible: false
+      }
+  },
+  methods: {
+      toggleNav(){
+          this.isVisible = !this.isVisible
+      }
+  }
+}
+</script>
