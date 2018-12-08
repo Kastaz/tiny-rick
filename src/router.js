e22fc902ac6403224182ac1c87ff298bf2307393
kamil stawski <stawski.kamil@gmail.com>
2018-12-08 17:05:54 +0100

    add episode path

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Dashboard from './views/Dashboard.vue'
+import Dashboard from './views/Dashboard/Dashboard.vue'
 
 Vue.use(Router)
 
@@ -13,13 +13,11 @@ export default new Router({
       name: 'dashboard',
       component: Dashboard
     },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (about.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    // }
+    {
+      path: '/episode/:id',
+      name: 'episodeDetails',
+      props: true,
+      component: () => import('./views/EpisodeDetails/EpisodeDetails.vue')
+    }
   ]
 })
