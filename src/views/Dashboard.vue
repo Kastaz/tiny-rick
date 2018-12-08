2df7202fdf2ed1bad482068f7e58e8a99af81bca
kamil stawski <stawski.kamil@gmail.com>
2018-12-08 12:37:54 +0100

    add dashboard component

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <TheHeader />
+    <main class="main">
+    <!-- content -->
+      <EpisodesList />
+    </main>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import TheHeader from '@/components/layout/TheHeader/TheHeader.vue';
+import EpisodesList from '@/components/episodesList/EpisodesList.vue';
+
+export default {
+  name: 'Dashboard',
+  components: {
+    TheHeader,
+    EpisodesList
+  }
+}
+</script>
