bd41191406c264b7df5a47b74d324e9c7b681104
kamil stawski <stawski.kamil@gmail.com>
2018-12-08 12:38:11 +0100

    Create EpisodesList.vue

---
+++ src/components/episodesList/EpisodesList.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h1 class="episodes__title">Episodes</h1>
+
+        <div class="episodes__search">
+            <!-- search icon -->
+            
+            <input type="text" placeholder="Search">
+        </div>
+
+        <ul class="episodes__list">
+            <li>
+                <!-- Episode Item -->
+            </li>
+            <!-- ... -->
+        </ul>
+
+        <div v-if="isLoading" class="episodes__loader">
+        Loading more
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            isLoading : false
+        } 
+    }
+}
+</script>
+
