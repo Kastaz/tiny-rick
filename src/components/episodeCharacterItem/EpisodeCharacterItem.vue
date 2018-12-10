587359a5a43cbbd32e2fa28c9fd3791ecd5bead7
kamil stawski <stawski.kamil@gmail.com>
2018-12-10 08:28:39 +0100

    Create EpisodeCharacterItem.vue

---
+++ src/components/episodeCharacterItem/EpisodeCharacterItem.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="base-item">
+        <header class="base-item__header">
+            <img :src="characterDetails.image" class="base-item__img">
+            <div class="base-item__header-content">
+            <h3 class="base-item__title">{{characterDetails.name}}</h3>
+            <span class="base-item__subtitle">{{characterDetails.species}} from {{characterDetails.origin.name}}</span>
+            </div>
+        </header>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        characterDetails: {
+            type: Object
+        }
+    },
+}
+</script>
